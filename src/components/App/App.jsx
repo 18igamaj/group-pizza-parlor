01a1764f85de1fdbbd9b535cf9826edfcf8f1e2f
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-08 14:02:39 -0500

    added get function and listItem,PizzaList, and Item render

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import PizzaList from '../PizzaList/PizzaList';
 
 function App() {
+  const dispatch = useDispatch
+const getPizza = () => {
+axios.get('/api/pizza') 
+.then ( response => {
+  console.log('response.data is ', response.data)
+dispatch ({type: 'SET_PIZZA', payload: response.data})
+})
+.catch(error => {
+  alert('There is a problem getting our pizzas :(')
+})
+}
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      <PizzaList getPizza={getPizza} />
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
