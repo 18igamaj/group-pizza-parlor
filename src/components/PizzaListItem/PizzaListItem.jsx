0bb84dc064afb7e1b8af2d6c3dc6425185a48c2c
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-09 09:36:33 -0500

    added dispatch to listitem

--- src/components/PizzaListItem/PizzaListItem.jsx
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -2,10 +2,13 @@ import {useDispatch} from 'react-redux'
 
 function PizzaListItem({slice, getPizza}){
 
-
+// we can dispatch the cart action
     const dispatch = useDispatch()
+    const addPizzaToCart =() =>{
+        
+    }
 
-
+//this is where we render our pizzas to dom
     return(
         <div>
             <img src={slice.image_path} />
